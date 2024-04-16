4fa2a60ef61731a93e3b60f98f53d78d7b06de5a
petarkosic <avt3914@gmail.com>
2024-04-16 23:42:47 +0200

    fix: docker compose file
    Add volumes to persist data and ensure proper initialization of databases.
    Add init sql scripts for command and query databases.
    Update wait-for-it script timeout.

---
+++ cqrs/query/db/01_init.sql
@@ -0,0 +1,8 @@
+\c cqrs;
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
